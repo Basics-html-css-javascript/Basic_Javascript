3a196446c7015895b727cfef8b9d9336aed09425
sharada417 <sharada.kanchana2001@gmail.com>
2025-08-05 21:42:05 +0530

    Loop question

---
+++ Javascript/Loops/Q1.js
@@ -0,0 +1,19 @@
+//Write a for loop to print all even numbers from 2 to 10.
+let x =0;
+for(let i=0; i<=10; i+=2){
+    console.log(i);
+}
+
+//Use a while loop to print numbers from 5 down to 1
+let j = 5;
+while(j>0){
+    console.log(j);
+    j--; 
+}
+
+//Write a do...while loop that runs at least once and prints "Hello
+let y = 1;
+do{
+ console.log("Hello");
+ y++;
+}while(y<=2);
