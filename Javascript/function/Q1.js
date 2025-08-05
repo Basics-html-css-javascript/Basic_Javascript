10f1d922b2ef6b30b757a006f9a30c2715dee479
sharada417 <sharada.kanchana2001@gmail.com>
2025-08-05 21:49:32 +0530

    function Q1

---
+++ Javascript/function/Q1.js
@@ -0,0 +1,11 @@
+// Convert this function to arrow function:
+// function greet(name) {
+//   return "Hello " + name;
+// }
+
+const greet = (name) => "Hello" +" " + name;
+console.log(greet("shanuka"));
+
+//Write an arrow function to calculate the square of a number.
+const squrtnum = (i) => i*i;
+console.log(squrtnum(5));
